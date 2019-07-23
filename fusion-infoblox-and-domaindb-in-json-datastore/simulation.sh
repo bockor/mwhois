f8b6772bb4d1444d02c50cfdaf2e16f4574e9829
bruno seys <bruno.on.the.road@gmail.com>
2019-07-23 11:24:53 +0200

    shell script to execute all simulation steps

---
+++ fusion-infoblox-and-domaindb-in-json-datastore/simulation.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+./nra-whois-networks-generator.py && \
+	./nra-whois-domaindb-csv-generator.py && \
+	./nra-whois-infoblox-generator.py && \
+	./nra-whois-domaindb-csv-to-json.py && \
+	./nra-whois-infoblox-domaindb-fusion.py && \
+	./nra-whois-datastore-load.py
